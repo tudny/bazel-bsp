7fb48639cb536ecaf7430ede96da2a58627cc0c8
Tudny <aleksanderwt@gmail.com>
2022-11-16 13:40:01 +0100

    Added simple Rust project for e2e testing

---
+++ e2e/test-resources/rust-project/dep_example/lib.rs
@@ -0,0 +1,4 @@
+
+pub fn fib(n: u32) -> u32 {
+    if n < 2 { n } else { fib(n - 1) + fib(n - 2) }
+}
---
+++ e2e/test-resources/rust-project/example/main.rs
@@ -0,0 +1,7 @@
+
+extern crate dep_example;
+
+fn main() {
+    println!("Hello, world!");
+    println!("fib(10) = {}", dep_example::fib(10));
+}
